f4418252697a5556d7fa46230e7b2fe6b41fcd13
Vlad <vivdobr@mail.ru>
2020-04-09 14:41:01 +0700

    Первое изменение 14:40 09.04.20

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//alert('Dobrynin');
+
+//console.log(second);
+/*var leftBorderWidth = 1;
+console.log(leftBorderWidth);
+let second = 2;
+const pi = 3.14;
+let Dobr = 111;
+console.log(Dobr);
+let obj = {
+  name: 'Vladimir',
+  faml: 'Dobrynin',
+  god: '65+'
+};
+console.log(obj.god); */
+//let arr = ['computer','printer','router'];
+//console.log(arr[1]);
+
+//alert('Dobrynin Vladimir');
+//let answer = confirm('Вам 65+ ?');
+//console.log(answer);
+
+let answer = prompt('Вам 65+ ?','Yes');
+console.log(answer);
+console.log(typeof(answer));
